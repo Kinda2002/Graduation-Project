9cc2cd556981ac31b23513202a3132f4b7815844
Kinda2002 <129109237+Kinda2002@users.noreply.github.com>
2025-08-28 11:03:52 +0300

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,127 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
+2025-08-28 10:46:59,370 [4] INFO MicroServicesHealthService.Utilities - ***Attempting GET Request to URL: https://mobileemail.arabbank.com.jo/DeviceManagement/Enrollment ***
+2025-08-28 10:46:59,823 [4] INFO MicroServicesHealthService.Utilities - ***Attempting GET Request to URL: https://mobileemail.arabbank.com.jo/Microsoft-Server-ActiveSync ***
+2025-08-28 10:46:59,932 [4] ERROR MicroServicesHealthService.Utilities - GetResponse WebException: 
+System.Net.WebException: The remote server returned an error: (401) Unauthorized.
+   at System.Net.HttpWebRequest.GetResponse()
+   at MicroServicesHealthService.Utilities.GetResponse(String url)
+2025-08-28 10:46:59,932 [4] INFO MicroServicesHealthService.Utilities - ***Attempting GET Request to URL: https://mobileemail.arabbank.com.jo/MailNotificationService/api/ens/registerdevicev2 ***
+2025-08-28 10:47:00,026 [4] ERROR MicroServicesHealthService.Utilities - GetResponse WebException: 
+System.Net.WebException: The remote server returned an error: (405) Method Not Allowed.
+   at System.Net.HttpWebRequest.GetResponse()
+   at MicroServicesHealthService.Utilities.GetResponse(String url)
+2025-08-28 10:47:00,026 [4] INFO MicroServicesHealthService.Utilities - ***Attempting GET Request to URL: https://NEOAPI.arabbank.com ***
+2025-08-28 10:47:01,354 [4] ERROR MicroServicesHealthService.Utilities - GetResponse WebException: 
+System.Net.WebException: The remote server returned an error: (404) Not Found.
+   at System.Net.HttpWebRequest.GetResponse()
+   at MicroServicesHealthService.Utilities.GetResponse(String url)
+2025-08-28 10:47:01,354 [4] INFO MicroServicesHealthService.Utilities - ***Attempting GET Request to URL: https://api.arabbank.com/ ***
+2025-08-28 10:47:01,900 [4] ERROR MicroServicesHealthService.Utilities - GetResponse WebException: 
+System.Net.WebException: The remote server returned an error: (404) Not Found.
+   at System.Net.HttpWebRequest.GetResponse()
+   at MicroServicesHealthService.Utilities.GetResponse(String url)
+2025-08-28 10:47:01,900 [4] INFO MicroServicesHealthService.Utilities - ***Attempting GET Request to URL: https://Webapps.arabbank.com ***
+2025-08-28 10:47:02,291 [4] INFO MicroServicesHealthService.Utilities - ***Attempting GET Request to URL: https://www.arabiconnect.com/iportalweb/iportal/jsps/orbilogin.jsp ***
+2025-08-28 10:47:02,760 [4] INFO MicroServicesHealthService.Utilities - ***Attempting GET Request to URL: https://www.arabiconnect.com/TF/Corp_index.htm ***
+2025-08-28 10:47:02,853 [4] INFO MicroServicesHealthService.Utilities - ***Attempting GET Request to URL: https://MB.arabbank.com ***
+2025-08-28 10:47:03,228 [4] INFO MicroServicesHealthService.Utilities - ***Attempting GET Request to URL: https://arabbank.com.jo/ ***
+2025-08-28 10:47:04,494 [4] INFO MicroServicesHealthService.Utilities - ***Attempting GET Request to URL: https://arabbank.com ***
+2025-08-28 10:47:06,025 [4] INFO MicroServicesHealthService.Utilities - ***Attempting GET Request to URL: https://arabbank.jo ***
+2025-08-28 10:47:07,227 [4] INFO MicroServicesHealthService.Utilities - ***Attempting GET Request to URL: https://arabbank.com.eg ***
+2025-08-28 10:47:08,649 [4] INFO MicroServicesHealthService.Utilities - ***Attempting GET Request to URL: https://arabbank.com.lb ***
+2025-08-28 10:47:09,836 [4] INFO MicroServicesHealthService.Utilities - ***Attempting GET Request to URL: https://arabbank.ps/ ***
+2025-08-28 10:47:10,930 [4] INFO MicroServicesHealthService.Utilities - ***Attempting GET Request to URL: https://arabbank.ae/ ***
+2025-08-28 10:47:12,336 [4] INFO MicroServicesHealthService.Utilities - ***Attempting GET Request to URL: https://arabbank.bh/ ***
+2025-08-28 10:47:13,460 [4] INFO MicroServicesHealthService.Utilities - ***Attempting GET Request to URL: https://arabbank.com.qa ***
+2025-08-28 10:47:17,460 [4] INFO MicroServicesHealthService.Utilities - ***Attempting GET Request to URL: https://arabbank.dz/ ***
+2025-08-28 10:47:18,553 [4] INFO MicroServicesHealthService.Utilities - ***Attempting GET Request to URL: https://arabbank.ma/ ***
+2025-08-28 10:47:21,490 [4] INFO MicroServicesHealthService.Utilities - ***Attempting GET Request to URL: https://arabi-sme.com/eg/smelending ***
+2025-08-28 10:47:21,990 [4] INFO MicroServicesHealthService.Utilities - ***Attempting GET Request to URL: https://arabiegift.com/egift-ui/egift-portal/experience/v1/merchant/list ***
+2025-08-28 10:47:23,005 [4] ERROR MicroServicesHealthService.Utilities - GetResponse WebException: 
+System.Net.WebException: The remote server returned an error: (401) Unauthorized.
+   at System.Net.HttpWebRequest.GetResponse()
+   at MicroServicesHealthService.Utilities.GetResponse(String url)
+2025-08-28 10:47:23,005 [4] INFO MicroServicesHealthService.Utilities - ***Attempting GET Request to URL: https://acabesinternational.com ***
+2025-08-28 10:47:23,943 [4] INFO MicroServicesHealthService.Utilities - ***Attempting GET Request to URL: https://webapps.arabbank.com/miniapps/ab/e-gift ***
+2025-08-28 10:47:24,349 [4] INFO MicroServicesHealthService.Utilities - ***Attempting GET Request to URL: https://webapps.arabbank.com/miniapps/ab/dmj-ui/ ***
+2025-08-28 10:47:24,505 [4] INFO MicroServicesHealthService.Utilities - ***Attempting GET Request to URL: https://webapps.arabbank.com/pb/customer-gateway ***
+2025-08-28 10:47:45,672 [4] ERROR MicroServicesHealthService.Utilities - GetResponse WebException: 
+System.Net.WebException: Unable to connect to the remote server ---> System.Net.Sockets.SocketException: A connection attempt failed because the connected party did not properly respond after a period of time, or established connection failed because connected host has failed to respond 35.246.210.234:1080
+   at System.Net.Sockets.Socket.DoConnect(EndPoint endPointSnapshot, SocketAddress socketAddress)
+   at System.Net.ServicePoint.ConnectSocketInternal(Boolean connectFailure, Socket s4, Socket s6, Socket& socket, IPAddress& address, ConnectSocketState state, IAsyncResult asyncResult, Exception& exception)
+   --- End of inner exception stack trace ---
+   at System.Net.HttpWebRequest.GetResponse()
+   at MicroServicesHealthService.Utilities.GetResponse(String url)
+2025-08-28 10:47:45,688 [4] INFO MicroServicesHealthService.Utilities - ***Attempting GET Request to URL: https://webapps.arabbank.com/pb/janus ***
+2025-08-28 10:47:45,782 [4] ERROR MicroServicesHealthService.Utilities - GetResponse WebException: 
+System.Net.WebException: The remote server returned an error: (404) Not Found.
+   at System.Net.HttpWebRequest.GetResponse()
+   at MicroServicesHealthService.Utilities.GetResponse(String url)
+2025-08-28 10:47:45,782 [4] INFO MicroServicesHealthService.Utilities - ***Attempting GET Request to URL: https://webapps.arabbank.com/miniapps/ab/jo/ab-invest-ui ***
+2025-08-28 10:47:45,985 [4] INFO MicroServicesHealthService.Utilities - ***Attempting GET Request to URL: https://webapps.arabbank.com/miniapps/ab/jo/bnpl-ui ***
+2025-08-28 10:47:46,172 [4] INFO MicroServicesHealthService.Utilities - ***Attempting GET Request to URL: https://webapps.arabbank.com/miniapps/ab/staff-loan-ui/ ***
+2025-08-28 10:47:46,281 [4] INFO MicroServicesHealthService.Utilities - ***Attempting GET Request to URL: https://webapps.arabbank.com/miniapps/ab/daman-pay ***
+2025-08-28 10:47:46,469 [4] INFO MicroServicesHealthService.Utilities - ***Attempting GET Request to URL: https://webapps.arabbank.com/miniapps/ab/jo/sanad ***
+2025-08-28 10:47:46,656 [4] INFO MicroServicesHealthService.Utilities - ***Attempting GET Request to URL: https://webapps.arabbank.com/miniapps/ab/chatbot-ui/ ***
+2025-08-28 10:47:46,781 [4] INFO MicroServicesHealthService.Utilities - ***Attempting GET Request to URL: https://webapps.arabbank.com/miniapps/ab/salary-advance-loans-eg/ ***
+2025-08-28 10:47:46,875 [4] INFO MicroServicesHealthService.Utilities - ***Attempting GET Request to URL: https://webapps.arabbank.com/miniapps/ab/ae/onboarding ***
+2025-08-28 10:47:46,984 [4] ERROR MicroServicesHealthService.Utilities - GetResponse WebException: 
+System.Net.WebException: The remote server returned an error: (404) Not Found.
+   at System.Net.HttpWebRequest.GetResponse()
+   at MicroServicesHealthService.Utilities.GetResponse(String url)
+2025-08-28 10:47:46,984 [4] INFO MicroServicesHealthService.Utilities - ***Attempting GET Request to URL: https://webapps.arabbank.com/miniapps/ab/jo/onboarding ***
+2025-08-28 10:47:47,172 [4] INFO MicroServicesHealthService.Utilities - ***Attempting GET Request to URL: https://webapps.arabbank.com/ab/pb/tnc ***
+2025-08-28 10:47:47,265 [4] INFO MicroServicesHealthService.Utilities - ***Attempting GET Request to URL: https://mortgage.arabi-miniapp.com/insurance-ui/ ***
+2025-08-28 10:47:47,656 [4] INFO MicroServicesHealthService.Utilities - ***Attempting GET Request to URL: https://www.reflectapp.com ***
+2025-08-28 10:47:50,721 [4] INFO MicroServicesHealthService.Utilities - ***Attempting GET Request to URL: https://dz00-sigweb/webret/WebRetrieve.dll ***
+2025-08-28 10:47:53,017 [4] ERROR MicroServicesHealthService.Utilities - GetResponse WebException: 
+System.Net.WebException: The remote name could not be resolved: 'dz00-sigweb'
+   at System.Net.HttpWebRequest.GetResponse()
+   at MicroServicesHealthService.Utilities.GetResponse(String url)
+2025-08-28 10:47:53,017 [4] INFO MicroServicesHealthService.Utilities - ***Attempting GET Request to URL: https://mobile.mobicashpayments.com ***
+2025-08-28 10:47:53,611 [4] INFO MicroServicesHealthService.Utilities - ***Attempting GET Request to URL: https://10.0.32.4:5001/ ***
+2025-08-28 10:48:14,671 [4] ERROR MicroServicesHealthService.Utilities - GetResponse WebException: 
+System.Net.WebException: Unable to connect to the remote server ---> System.Net.Sockets.SocketException: A connection attempt failed because the connected party did not properly respond after a period of time, or established connection failed because connected host has failed to respond 10.0.32.4:5001
+   at System.Net.Sockets.Socket.DoConnect(EndPoint endPointSnapshot, SocketAddress socketAddress)
+   at System.Net.ServicePoint.ConnectSocketInternal(Boolean connectFailure, Socket s4, Socket s6, Socket& socket, IPAddress& address, ConnectSocketState state, IAsyncResult asyncResult, Exception& exception)
+   --- End of inner exception stack trace ---
+   at System.Net.HttpWebRequest.GetResponse()
+   at MicroServicesHealthService.Utilities.GetResponse(String url)
+2025-08-28 10:48:14,687 [4] INFO MicroServicesHealthService.Utilities - Method Start --GetCmonMobileCookies-- 
+2025-08-28 10:48:16,749 [4] INFO MicroServicesHealthService.Utilities - Before Calling Cmon Mobile
+2025-08-28 10:48:16,764 [4] ERROR MicroServicesHealthService.Utilities - SendToCmonMobile Ex: 
+System.IO.FileLoadException: Could not load file or assembly 'RestSharp, Version=108.0.1.0, Culture=neutral, PublicKeyToken=598062e77f915f75' or one of its dependencies. The located assembly's manifest definition does not match the assembly reference. (Exception from HRESULT: 0x80131040)
+File name: 'RestSharp, Version=108.0.1.0, Culture=neutral, PublicKeyToken=598062e77f915f75'
+   at MicroServicesHealthService.Utilities.ExecuteFormBodyPostRequest(String url, Dictionary`2 headers, Dictionary`2 parameters)
+   at MicroServicesHealthService.Utilities.SendToCmonMobile(List`1 details)
 
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
+WRN: Assembly binding logging is turned OFF.
+To enable assembly bind failure logging, set the registry value [HKLM\Software\Microsoft\Fusion!EnableLog] (DWORD) to 1.
+Note: There is some performance penalty associated with assembly bind failure logging.
+To turn this feature off, remove the registry value [HKLM\Software\Microsoft\Fusion!EnableLog].
 
-import 'package:pro/main.dart';
+2025-08-28 10:48:16,764 [4] INFO MicroServicesHealthService.Utilities - Method Start --GetCmonMobileCookies-- 
+2025-08-28 10:48:16,858 [4] INFO MicroServicesHealthService.Utilities - Before Calling Cmon Mobile
+2025-08-28 10:48:16,858 [4] ERROR MicroServicesHealthService.Utilities - SendToCmonMobile Ex: 
+System.IO.FileLoadException: Could not load file or assembly 'RestSharp, Version=108.0.1.0, Culture=neutral, PublicKeyToken=598062e77f915f75' or one of its dependencies. The located assembly's manifest definition does not match the assembly reference. (Exception from HRESULT: 0x80131040)
+File name: 'RestSharp, Version=108.0.1.0, Culture=neutral, PublicKeyToken=598062e77f915f75'
+   at MicroServicesHealthService.Utilities.ExecuteFormBodyPostRequest(String url, Dictionary`2 headers, Dictionary`2 parameters)
+   at MicroServicesHealthService.Utilities.SendToCmonMobile(List`1 details)
 
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+WRN: Assembly binding logging is turned OFF.
+To enable assembly bind failure logging, set the registry value [HKLM\Software\Microsoft\Fusion!EnableLog] (DWORD) to 1.
+Note: There is some performance penalty associated with assembly bind failure logging.
+To turn this feature off, remove the registry value [HKLM\Software\Microsoft\Fusion!EnableLog].
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+2025-08-28 10:48:16,858 [4] INFO MicroServicesHealthService.Utilities - Method Start --GetCmonMobileCookies-- 
+2025-08-28 10:48:18,092 [4] INFO MicroServicesHealthService.Utilities - Before Calling Cmon Mobile
+2025-08-28 10:48:18,092 [4] ERROR MicroServicesHealthService.Utilities - SendToCmonMobile Ex: 
+System.IO.FileLoadException: Could not load file or assembly 'RestSharp, Version=108.0.1.0, Culture=neutral, PublicKeyToken=598062e77f915f75' or one of its dependencies. The located assembly's manifest definition does not match the assembly reference. (Exception from HRESULT: 0x80131040)
+File name: 'RestSharp, Version=108.0.1.0, Culture=neutral, PublicKeyToken=598062e77f915f75'
+   at MicroServicesHealthService.Utilities.ExecuteFormBodyPostRequest(String url, Dictionary`2 headers, Dictionary`2 parameters)
+   at MicroServicesHealthService.Utilities.SendToCmonMobile(List`1 details)
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+WRN: Assembly binding logging is turned OFF.
+To enable assembly bind failure logging, set the registry value [HKLM\Software\Microsoft\Fusion!EnableLog] (DWORD) to 1.
+Note: There is some performance penalty associated with assembly bind failure logging.
+To turn this feature off, remove the registry value [HKLM\Software\Microsoft\Fusion!EnableLog].
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
-}
